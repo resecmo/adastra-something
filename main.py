4b28102add7ddb38138c3c3565c75a847b637bb3
resecmo <resecmo@gmail.com>
2021-08-26 20:13:53 +0300

    Added simple loading of SNPs from ADASTRA

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import json 
+import json
+import requests
 
-with open("config.json") as config:
-    print(json.load(config))
+with open("config.json") as config_file:
+    config = json.load(config_file)
+gene_name = config["gene_name"]
+
+
+r = requests.get(f'https://adastra.autosome.ru/api/v3/search/snps/gene_name/{gene_name}')
+if r.status_code == 200:
+    resp = json.loads(r.text)
+else:
+    print(f"status code: {r.status_code}")
+
+
+
+print(json.dumps(resp, indent=2))
 
