fedb81f1e045e7f84096fce18e4fc2790308fc76
resecmo <resecmo@gmail.com>
2021-08-28 20:43:35 +0300

    search for intersection

--- main.py
+++ main.py
@@ -18,10 +18,19 @@ def fetch_adastra_snps(gene_name):
         #print(f'{snp["chromosome"]}:{snp["position"]}')
         positions.append((snp["chromosome"], snp["position"]))
     return positions
-print(fetch_adastra_snps(config["gene_name"]))
+adastra_snps = fetch_adastra_snps(config["gene_name"])
+print(f"{len(adastra_snps)} SNPs in ADASTRA")
+print(adastra_snps)
 
 
 with open(config["path_to_bed"]) as f:
     bed_lines = f.readlines()
+    print(f"{len(bed_lines)} entries in BED")
+    for bed_line in bed_lines:
+        l = bed_line.split("\t")
+        l[-1] = l[-1][:-1]
+        for snp in adastra_snps:
+            if (snp[0] == l[0]) and (int(l[1]) <= snp[1] <= int(l[2])):
+                print(snp, l)
 
 
