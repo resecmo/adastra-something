542ed2dd177c52ba5e7a6536301dbd1a932852c7
Vasily <resecmo@gmail.com>
2021-11-10 14:40:36 +0300

    added base url

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from os import system
 
 
 def fetch_adastra_snps(gene_name):
-    r = requests.get(f'https://adastra.autosome.ru/api/v4/search/snps/gene_name/{gene_name}')
+    r = requests.get(config['base_api_url'] + f'search/snps/gene_name/{gene_name}')
     if r.status_code == 200:
         resp = json.loads(r.text)
     else:
